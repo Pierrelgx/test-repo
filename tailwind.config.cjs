7d899ee9be2801f90e4a38095052d86b42ce2ab0
Pierrooow <pl.informatique47@gmail.com>
2024-07-15 19:51:15 +0200

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      animation: {
+        marquee: "marquee 50s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: {
+            transform: "translateX(0)",
+          },
+          to: {
+            transform: "translateX(calc(-100% - 2.5rem))",
+          },
+        },
+      },
+      colors: {
+        primary: '#ff6210',
+        secondary: '#14171a',
+        accent: '#657786',
+        background: '#f5f8fa',
+        border: '#e1e8ed',
+        customGreen: '#00ff00', // Exemple de couleur personnalisée
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
